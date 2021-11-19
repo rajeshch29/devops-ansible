3e5097332297e7b5df57254ad0569f490747ab92
rajeshch29 <44319304+rajeshch29@users.noreply.github.com>
2021-11-19 19:32:11 +0530

    Update server.tf

--- server.tf
+++ server.tf
@@ -3,7 +3,7 @@ region = "us-east-2"
 }
 resource "aws_instance" "myawsserver" {
   ami = "ami-077e31c4939f6a2f3"
-  instance_type = "t2.micro"
+  instance_type = "t2.small"
   key_name = "raj-key"
 
   tags = {
